f4e6fd9a3b8938d6f9b1953fe2a07baa2d0d80d0
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 19:33:35 +0200

    Get simple test, that should travis run

---
+++ tests/TravisTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+from unittest import TestCase, main
+
+
+class TravisTest(TestCase):
+    def test_shouldSucceed(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    main()
