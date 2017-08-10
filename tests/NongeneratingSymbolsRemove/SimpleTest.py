b242de3217ad9cf6a98ca2513ed1e4f66d2537ad
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 21:29:25 +0200

    Add simple test of removing nongenerating symbols

--- tests/NongeneratingSymbolsRemove/SimpleTest.py
+++ tests/NongeneratingSymbolsRemove/SimpleTest.py
@@ -12,8 +12,37 @@ from grammpy import *
 from grammpy_transforms import ContextFree
 
 
-class SimpleTest(TestCase):
+class A(Nonterminal):
     pass
+
+
+class B(Nonterminal):
+    pass
+
+
+class C(Nonterminal):
+    pass
+
+
+class RuleAto0B(Rule):
+    fromSymbol = A
+    right = [0, B]
+
+
+class RuleBto1(Rule):
+    fromSymbol = B
+    toSymbol = 1
+
+
+class SimpleTest(TestCase):
+    def test_simpleTest(self):
+        g = Grammar(terminals=[0, 1],
+                    nonterminals=[A, B, C],
+                    rules=[RuleAto0B, RuleBto1])
+        changed = ContextFree.remove_nongenerastingSymbols(g)
+        self.assertTrue(changed.have_term([0, 1]))
+        self.assertTrue(changed.have_nonterm([A, B]))
+        self.assertFalse(changed.have_nonterm(C))
 
 
 if __name__ == '__main__':
