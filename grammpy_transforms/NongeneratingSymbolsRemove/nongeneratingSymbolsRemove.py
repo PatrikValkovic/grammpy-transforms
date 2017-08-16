5ad23fde074fde0f5efeeba847521d2269ae4f8e
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-16 18:47:12 +0200

    Basically implement method for removing nongenerating symbols

--- grammpy_transforms/NongeneratingSymbolsRemove/nongeneratingSymbolsRemove.py
+++ grammpy_transforms/NongeneratingSymbolsRemove/nongeneratingSymbolsRemove.py
@@ -10,5 +10,34 @@ Part of grammpy-transforms
 from grammpy import Grammar
 
 
+def _copy_grammar(grammar):
+    return Grammar(terminals=grammar.terms(),
+                   nonterminals=grammar.nonterms(),
+                   rules=grammar.rules(),
+                   start_symbol=grammar.start_get())
+
+
 def remove_nongenerating_symbol(grammar: Grammar, transform_grammar=False) -> Grammar:
-    raise NotImplementedError()
+    if transform_grammar is True:
+        grammar = _copy_grammar(grammar)
+    generates = set(grammar.terms())
+    while True:
+        additional = generates.copy()
+        for rule in grammar.rules():
+            rightPart = rule.right
+            allIn = True
+            for symbol in rightPart:
+                if symbol not in generates:
+                    allIn = False
+            if not allIn:
+                continue
+            for symbol in rule.left:
+                additional.add(symbol)
+        if additional == generates:
+            break
+        generates = additional
+    allNonterms = list(grammar.nonterms())
+    for nonterm in allNonterms:
+        if nonterm not in generates:
+            grammar.remove_nonterm(nonterm)
+    return grammar
