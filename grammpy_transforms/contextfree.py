3072b6dd724255c3fba3bc6f837ec823d507e321
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 14:38:30 +0200

    Implement alg to remove useless symbols

--- grammpy_transforms/contextfree.py
+++ grammpy_transforms/contextfree.py
@@ -18,12 +18,23 @@ class ContextFree:
         return remove_nongenerating_symbols(grammar, transform_grammar)
 
     @staticmethod
-    def is_grammar_generating(grammar: Grammar, tranform_gramar=False, perform_remove=True):
+    def is_grammar_generating(grammar: Grammar, transform_grammar=False, perform_remove=True):
         g = grammar
         if perform_remove:
-            g = ContextFree.remove_nongenerating_symbols(grammar, transform_grammar=tranform_gramar)
+            g = ContextFree.remove_nongenerating_symbols(grammar, transform_grammar=transform_grammar)
         return g.start_get() in g.nonterms()
 
     @staticmethod
     def remove_unreachable_symbols(grammar: Grammar, transform_grammar=False):
         return remove_unreachable_symbols(grammar, transform_grammar)
+
+    @staticmethod
+    def remove_useless_symbols(grammar: Grammar, transform_grammar=False, *,
+                                   perform_unreachable_alg = True,
+                                   perform_nongenerating_alg = True) -> Grammar:
+        if perform_nongenerating_alg:
+            grammar = ContextFree.remove_nongenerating_symbols(grammar, transform_grammar=transform_grammar)
+            transform_grammar = False
+        if perform_unreachable_alg:
+            grammar = ContextFree.remove_unreachable_symbols(grammar, transform_grammar=transform_grammar)
+        return grammar
