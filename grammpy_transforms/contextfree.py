a633e7c1c5ca2fff4018ab7f51136ba9ad1b9cde
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 13:24:42 +0200

    Add additional parameters into CotextFree.is_grammar_generating method

--- grammpy_transforms/contextfree.py
+++ grammpy_transforms/contextfree.py
@@ -11,12 +11,14 @@ from grammpy import Grammar
 from .NongeneratingSymbolsRemove import remove_nongenerating_symbols
 
 
-class ContextFree():
+class ContextFree:
     @staticmethod
     def remove_nongenerating_symbols(grammar: Grammar, transform_grammar=False):
         return remove_nongenerating_symbols(grammar, transform_grammar)
 
     @staticmethod
-    def is_grammar_generating(grammar: Grammar):
-        g = ContextFree.remove_nongenerating_symbols(grammar)
+    def is_grammar_generating(grammar: Grammar, tranform_gramar=False, perform_remove=True):
+        g = grammar
+        if perform_remove:
+            g = ContextFree.remove_nongenerating_symbols(grammar, transform_grammar=tranform_gramar)
         return g.start_get() in g.nonterms()
