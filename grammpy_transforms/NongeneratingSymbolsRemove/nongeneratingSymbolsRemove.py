4ec8d8dbfb9da110de82511cd5165a1cee502bb8
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 12:05:45 +0200

    Add comments into code

--- grammpy_transforms/NongeneratingSymbolsRemove/nongeneratingSymbolsRemove.py
+++ grammpy_transforms/NongeneratingSymbolsRemove/nongeneratingSymbolsRemove.py
@@ -16,28 +16,37 @@ def _copy_grammar(grammar):
 
 
 def remove_nongenerating_symbol(grammar: Grammar, transform_grammar=False) -> Grammar:
-    if transform_grammar is False:
-        grammar = _copy_grammar(grammar)
+    # Copy if required
+    if transform_grammar is False: grammar = _copy_grammar(grammar)
+    # Create working sets
     generates = set(item.s for item in grammar.terms())
     rules = set(rule for rule in grammar.rules())
     while True:
+        # Create set of next iteration
         additional = generates.copy()
         processedRules = []
+        # Iterate over unprocessed rules
         for rule in rules:
             rightPart = rule.right
             allIn = True
+            # Check if all symbols on the right part of rule are in generates set
             for symbol in rightPart:
                 if symbol not in generates:
                     allIn = False
-            if not allIn:
-                continue
+                    break
+            # Symbol is missing so rule is not process
+            if not allIn: continue
+            # Rule is process - remove it from processing rules and make symbol as generating
             additional.add(rule.fromSymbol)
             processedRules.append(rule)
-        for item in processedRules:
-            rules.remove(item)
-        if additional == generates:
-            break
+        # End of rules iterations
+        # Remove process rules in current iteration
+        for item in processedRules: rules.remove(item)
+        # If current and previous iterations are same, than end iterations
+        if additional == generates: break
+        # Swap sets from previous and current iterations
         generates = additional
+    # Remove nonterms that are not generating
     for nonterm in set(grammar.nonterms()).difference(generates):
         grammar.remove_nonterm(nonterm)
     return grammar
