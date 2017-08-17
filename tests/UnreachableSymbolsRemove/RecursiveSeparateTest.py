2623da1f331eee9806501e53da1c5b8d0a80e2dd
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 13:54:56 +0200

    Add test of removing unreachable symbols in recursive grammar

---
+++ tests/UnreachableSymbolsRemove/RecursiveSeparateTest.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 8/17/17 1:51 PM
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+
+from unittest import main, TestCase
+from grammpy import *
+from grammpy_transforms import *
+
+
+class A(Nonterminal): pass
+class B(Nonterminal): pass
+class C(Nonterminal): pass
+class D(Nonterminal): pass
+class E(Nonterminal): pass
+class F(Nonterminal): pass
+class RuleAto0B(Rule): rule = ([A], [0, B])
+class RuleBto1C(Rule): rule = ([B], [1, C])
+class RuleCto01(Rule): rule = ([C], [0, 1])
+class RuleDto0E(Rule): rule = ([D], [0, E])
+class RuleEto1F(Rule): rule = ([E], [1, F])
+class RuleFto0D(Rule): rule = ([F], [0, D])
+
+
+
+
+class RecursiveSeparateTest(TestCase):
+    def test_simpleTest(self):
+        g = Grammar(terminals=[0, 1],
+                    nonterminals=[A, B, C, D, E, F],
+                    rules=[RuleAto0B, RuleBto1C, RuleCto01,
+                           RuleDto0E, RuleEto1F, RuleFto0D],
+                    start_symbol=A)
+        com = ContextFree.remove_unreachable_symbols(g)
+        self.assertTrue(com.have_term([0, 1]))
+        self.assertTrue(com.have_nonterm([A, B, C]))
+        self.assertFalse(com.have_nonterm(D))
+        self.assertFalse(com.have_nonterm(E))
+        self.assertFalse(com.have_nonterm(F))
+
+    def test_simpleTestShouldNotChange(self):
+        g = Grammar(terminals=[0, 1],
+                    nonterminals=[A, B, C, D, E, F],
+                    rules=[RuleAto0B, RuleBto1C, RuleCto01,
+                           RuleDto0E, RuleEto1F, RuleFto0D],
+                    start_symbol=A)
+        ContextFree.remove_unreachable_symbols(g)
+        self.assertTrue(g.have_term([0, 1]))
+        self.assertTrue(g.have_nonterm([A, B, C, D, E, F]))
+
+    def test_simpleTestShouldChange(self):
+        g = Grammar(terminals=[0, 1],
+                    nonterminals=[A, B, C, D, E, F],
+                    rules=[RuleAto0B, RuleBto1C, RuleCto01,
+                           RuleDto0E, RuleEto1F, RuleFto0D],
+                    start_symbol=A)
+        ContextFree.remove_unreachable_symbols(g, transform_grammar=True)
+        self.assertTrue(g.have_term([0, 1]))
+        self.assertTrue(g.have_nonterm([A, B, C]))
+        self.assertFalse(g.have_nonterm(D))
+        self.assertFalse(g.have_nonterm(E))
+        self.assertFalse(g.have_nonterm(F))
+
+
+if __name__ == '__main__':
+    main()
