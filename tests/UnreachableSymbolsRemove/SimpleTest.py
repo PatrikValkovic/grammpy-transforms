95ab6c0c23f756372a3e34e4386c15b4beaf0aa7
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 13:33:45 +0200

    Add file for simple test of unreachable symbols remove

---
+++ tests/UnreachableSymbolsRemove/SimpleTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+from unittest import main, TestCase
+from grammpy import *
+from grammpy_transforms import *
+
+
+class SimpleTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
