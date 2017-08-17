6e78c511e3374886249fcb885162a95987ccc037
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 13:19:32 +0200

    Implement ContextFree.is_grammar_generating method

--- grammpy_transforms/contextfree.py
+++ grammpy_transforms/contextfree.py
@@ -15,3 +15,8 @@ class ContextFree():
     @staticmethod
     def remove_nongenerating_symbols(grammar: Grammar, transform_grammar=False):
         return remove_nongenerating_symbols(grammar, transform_grammar)
+
+    @staticmethod
+    def is_grammar_generating(grammar: Grammar):
+        g = ContextFree.remove_nongenerating_symbols(grammar)
+        return g.start_get() in g.nonterms()
