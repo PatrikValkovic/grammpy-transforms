a784fd2f12328c9a200c48e72d68ca1ff3709316
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 12:17:16 +0200

    Add file for test if is grammar generating

---
+++ tests/IsGrammarGeneratingTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+from unittest import TestCase, main
+from grammpy import *
+from grammpy_transforms import *
+
+
+class IsGrammarGeneratingTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
