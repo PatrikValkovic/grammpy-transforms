a002a9156ec92e8c89b984531a0417a8ca840654
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-20 18:04:21 +0200

    Add simple test of removing rules with epsilon

---
+++ tests/RemoveEpsilonRules/SimpleTest.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 20.08.2017 16:01
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+from unittest import TestCase, main
+from grammpy import *
+from grammpy_transforms import ContextFree
+from inspect import isclass
+
+class S(Nonterminal): pass
+class A(Nonterminal): pass
+class B(Nonterminal): pass
+class C(Nonterminal): pass
+class Rules(Rule):
+    rules = [
+        ([S], [1, B]),
+        ([A], [1, B]),
+        ([A], [EPS]),
+        ([B], [EPS]),
+        ([B], [1, C]),
+        ([C], [1, 1])]
+
+"""
+S->1B   A->1B   A->eps  B->eps  B->1C   C->11
+ToEpsilon: A,B
+S->1B   A->1B   A->eps  B->eps  B->1C   C->11   S->1    A->1
+                ------  ------                  ++++    ++++
+"""
+
+
+class SimpleTest(TestCase):
+    def test_simpleTest(self):
+        g = Grammar(terminals=[1],
+                    nonterminals=[S, A, B, C],
+                    rules=[Rules])
+        com = ContextFree.remove_rules_with_epsilon(g)
+        self.assertEqual(len(com.rules(include_inactive=True)), 8)
+        self.assertEqual(com.rules_count(), 6)
+        class RuleNewS(Rule): rule=([S], [1])
+        class RuleNewA(Rule): rule=([A], [1])
+        self.assertTrue(com.have_rule(RuleNewS))
+        self.assertTrue(com.have_rule(RuleNewA))
+        fromS = com.get_rule(RuleNewS)
+        self.assertEqual(fromS.fromSymbol, S)
+        self.assertEqual(fromS.toSymbol, 1)
+        self.assertTrue(isclass(fromS))
+        self.assertTrue(issubclass(fromS, ContextFree.EpsilonRemovedRule))
+        self.assertEqual(fromS.from_rule.rule, ([S], [1, B]))
+        self.assertEqual(fromS.replace_index, 1)
+        fromA = com.get_rule(RuleNewA)
+        self.assertEqual(fromS.fromSymbol, A)
+        self.assertEqual(fromS.toSymbol, 1)
+        self.assertTrue(isclass(fromA))
+        self.assertTrue(issubclass(fromA, ContextFree.EpsilonRemovedRule))
+        self.assertEqual(fromA.from_rule.rule, ([A], [1, B]))
+        self.assertEqual(fromA.replace_index, 1)
+        class OldA(Rule): rule=([A], [EPS])
+        class OldB(Rule): rule=([B], [EPS])
+        self.assertTrue(com.have_rule(OldA))
+        realA = com.get_rule(OldA)
+        self.assertFalse(realA._active)
+        self.assertTrue(com.have_rule(OldB))
+        realB = com.get_rule(OldB)
+        self.assertFalse(realB._active)
+
+    def test_simpleTestShouldNotChange(self):
+        g = Grammar(terminals=[1],
+                    nonterminals=[S, A, B, C],
+                    rules=[Rules])
+        ContextFree.remove_rules_with_epsilon(g)
+        self.assertEqual(len(g.rules(include_inactive=True)), 6)
+        self.assertEqual(g.rules_count(), 6)
+        class RuleNewS(Rule): rule=([S], [1])
+        class RuleNewA(Rule): rule=([A], [1])
+        self.assertFalse(g.have_rule(RuleNewS))
+        self.assertFalse(g.have_rule(RuleNewA))
+        class OldA(Rule): rule=([A], [EPS])
+        class OldB(Rule): rule=([B], [EPS])
+        self.assertTrue(g.have_rule(OldA))
+        realA = g.get_rule(OldA)
+        self.assertTrue(realA._active)
+        self.assertTrue(g.have_rule(OldB))
+        realB = g.get_rule(OldB)
+        self.assertTrue(realB._active)
+
+    def test_simpleTestShouldChange(self):
+        g = Grammar(terminals=[1],
+                    nonterminals=[S, A, B, C],
+                    rules=[Rules])
+        ContextFree.remove_rules_with_epsilon(g, transform_grammar=True)
+        self.assertEqual(len(g.rules(include_inactive=True)), 8)
+        self.assertEqual(g.rules_count(), 6)
+        class RuleNewS(Rule): rule=([S], [1])
+        class RuleNewA(Rule): rule=([A], [1])
+        self.assertTrue(g.have_rule(RuleNewS))
+        self.assertTrue(g.have_rule(RuleNewA))
+        fromS = g.get_rule(RuleNewS)
+        self.assertEqual(fromS.fromSymbol, S)
+        self.assertEqual(fromS.toSymbol, 1)
+        self.assertTrue(isclass(fromS))
+        self.assertTrue(issubclass(fromS, ContextFree.EpsilonRemovedRule))
+        self.assertEqual(fromS.from_rule.rule, ([S], [1, B]))
+        self.assertEqual(fromS.replace_index, 1)
+        fromA = g.get_rule(RuleNewA)
+        self.assertEqual(fromS.fromSymbol, A)
+        self.assertEqual(fromS.toSymbol, 1)
+        self.assertTrue(isclass(fromA))
+        self.assertTrue(issubclass(fromA, ContextFree.EpsilonRemovedRule))
+        self.assertEqual(fromA.from_rule.rule, ([A], [1, B]))
+        self.assertEqual(fromA.replace_index, 1)
+        class OldA(Rule): rule=([A], [EPS])
+        class OldB(Rule): rule=([B], [EPS])
+        self.assertTrue(g.have_rule(OldA))
+        realA = g.get_rule(OldA)
+        self.assertFalse(realA._active)
+        self.assertTrue(g.have_rule(OldB))
+        realB = g.get_rule(OldB)
+        self.assertFalse(realB._active)
+
+
+
+if __name__ == '__main__':
+    main()
