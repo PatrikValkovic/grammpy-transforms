e56e9cc31f659b66b4f037e5f7a5f90af34fd188
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-22 11:42:19 +0200

    Add methods to remove unit rules

---
+++ grammpy_transforms/UnitRulesRemove/findReachableSymbolsByUnitRules.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 22.08.2017 11:39
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+from types import Dict, List
+from grammpy import Nonterminal
+
+
+def find_reachables_symbols_by_unit_rules(grammar) \
+        -> Dict[Nonterminal, List[Nonterminal]]:
+    raise NotImplementedError()
---
+++ grammpy_transforms/UnitRulesRemove/unitRulesRemove.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 22.08.2017 11:37
+:Licence GNUv3
+Part of grammpy-transforms
+
+"""
+
+from grammpy import *
+
+
+def remove_unit_rules(grammar: Grammar, transform_grammar=False) -> Grammar:
+    raise NotImplementedError()
