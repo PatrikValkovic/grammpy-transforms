9f8dda297964349fc36d6274bbe537a7e8a7605b
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-22 14:01:31 +0200

    Add ReducedUnitRule class

--- grammpy_transforms/UnitRulesRemove/remove_unit_rules.py
+++ grammpy_transforms/UnitRulesRemove/remove_unit_rules.py
@@ -7,7 +7,12 @@ Part of grammpy-transforms
 
 """
 
+from typing import List
 from grammpy import *
+
+class ReducedUnitRule(Rule):
+    by_rules = []  # type: List[Rule]
+    end_rule = None  # type: Rule
 
 
 def remove_unit_rules(grammar: Grammar, transform_grammar=False) -> Grammar:
