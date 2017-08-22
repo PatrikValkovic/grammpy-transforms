ebafae6f1770639ffa20edf1e28ad1a25878c2c5
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-22 21:47:03 +0200

    Implement base algorithm to find unit rules

--- grammpy_transforms/UnitRulesRemove/find_symbols_reachable_by_unit_rules.py
+++ grammpy_transforms/UnitRulesRemove/find_symbols_reachable_by_unit_rules.py
@@ -8,10 +8,13 @@ Part of grammpy-transforms
 """
 
 from typing import List
-from grammpy import Nonterminal, Rule
+from grammpy import Grammar
 
 
 class UnitSymbolRechablingResults:
+    def __init__(self, field, translation):
+        raise NotImplementedError()
+
     def reach(self, from_symbol: Nonterminal, to_symbol: Nonterminal) -> bool:
         raise NotImplementedError()
 
@@ -22,5 +25,35 @@ class UnitSymbolRechablingResults:
         raise NotImplementedError()
 
 
-def find_nonterminals_reachable_by_unit_rules(grammar) -> UnitSymbolRechablingResults:
-    raise NotImplementedError()
+def find_nonterminals_reachable_by_unit_rules(grammar: Grammar) -> UnitSymbolRechablingResults:
+    # Get nonterms
+    nonterms = grammar.nonterms()
+    l = len(nonterms)
+    # Create indexes for nonterms
+    nonterm_to_index = dict()
+    for i in range(l):
+        nonterm_to_index[nonterms[i]] = i
+    # Prepare field
+    field = [[] for _ in nonterms]
+    for i in range(l):
+        for j in range(l):
+            field[i][j] = None
+    # Fill diagonal
+    for i in range(l):
+        field[i][i] = list()
+    # Fill rules
+    for rule in grammar.rules():
+        if len(rule.left)==1 and len(rule.right)==1:
+            field[nonterm_to_index[rule.fromSymbol]][nonterm_to_index[rule.toSymbol]] = [rule]
+    # Floyd Warshall
+    f = field
+    for k in range(l):
+        for i in range(l):
+            for j in range(l):
+                if f[i][k] is not None and f[k][j] is not None:
+                    if f[i][j] is None or len(f[i][j]) > len(f[i][k]) + len(f[k][j]):
+                        f[i][j] = f[i][k] + f[k][j]
+    # Return results
+    return UnitSymbolRechablingResults(f, nonterm_to_index)
+
+
