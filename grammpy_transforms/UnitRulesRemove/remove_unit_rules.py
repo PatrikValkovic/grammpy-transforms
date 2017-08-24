f8f01077ca0af52d6971b7872742710960c42bb0
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-24 15:08:45 +0200

    Remove type hinting for remove_unit_rules file

--- grammpy_transforms/UnitRulesRemove/remove_unit_rules.py
+++ grammpy_transforms/UnitRulesRemove/remove_unit_rules.py
@@ -13,28 +13,31 @@ from typing import List
 from grammpy import *
 from .find_symbols_reachable_by_unit_rules import find_nonterminals_reachable_by_unit_rules
 
+
 class ReducedUnitRule(Rule):
     by_rules = []  # type: List[Rule]
     end_rule = None  # type: Rule
 
 
-def _is_unit(rule: Rule) -> bool:
+def _is_unit(rule):
     return len(rule.left) == 1 and len(rule.right) == 1 and \
            isclass(rule.fromSymbol) and isclass(rule.toSymbol) and \
            issubclass(rule.fromSymbol, Nonterminal) and issubclass(rule.toSymbol, Nonterminal)
 
-def _create_rule(path: List[Rule], rule: Rule) -> ReducedUnitRule:
-    created = type("Reduced"+rule.__name__,(ReducedUnitRule,),ReducedUnitRule.__dict__.copy())
+
+def _create_rule(path, rule):
+    created = type("Reduced" + rule.__name__, (ReducedUnitRule,), ReducedUnitRule.__dict__.copy())
     created.rule = ([path[0].fromSymbol], rule.right)
     created.end_rule = rule
     created.by_rules = path
     return created
 
-def remove_unit_rules(grammar: Grammar, transform_grammar=False) -> Grammar:
+
+def remove_unit_rules(grammar, transform_grammar=False):
     if transform_grammar is False: grammar = copy(grammar)
-    #get connections
+    # get connections
     res = find_nonterminals_reachable_by_unit_rules(grammar)
-    #iterate through rules
+    # iterate through rules
     for rule in grammar.rules():
         if _is_unit(rule):
             grammar.remove_rule(rule)
@@ -45,4 +48,3 @@ def remove_unit_rules(grammar: Grammar, transform_grammar=False) -> Grammar:
                 created = _create_rule(path, rule)
                 grammar.add_rule(created)
     return grammar
-
