940c18ec942a22c01d122c1427515c55f3f16990
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-09-03 16:37:05 +0200

    Implemented restoring of epsilon rules

--- grammpy_transforms/EpsilonRulesRemove/epsilon_rules_restore.py
+++ grammpy_transforms/EpsilonRulesRemove/epsilon_rules_restore.py
@@ -8,7 +8,49 @@ Part of grammpy-transforms
 """
 
 from grammpy import *
+from .remove_rules_with_epsilon import EpsilonRemovedRule
+
+class Adding:
+    def __init__(self, rule: Rule):
+        self.rule = rule
+        self.processed = False
+
+    def process(self):
+        child_symbols = self.rule.to_symbols
+        self.processed = True
+        child_rules = []
+        for child in child_symbols:  # type: Nonterminal
+            if child.to_rule is not None:
+                child_rules.append(child.to_rule)
+        return child_rules
 
 
 def epsilon_rules_restore(root: Nonterminal):
-    raise NotImplementedError()
+    stack = list()
+    stack.append(Adding(root.to_rule))
+    while len(stack) > 0:
+        proc = stack.pop()  # type: Adding
+        if not proc.processed:
+            add = proc.process()
+            stack.append(proc)
+            for a in add:
+                stack.append(Adding(a))
+        elif isinstance(proc.rule, EpsilonRemovedRule):
+            created_rule = proc.rule.from_rule()  # type: Rule
+            #Solve parents
+            for s in proc.rule.from_symbols:  # type: Nonterminal
+                s._set_to_rule(created_rule)
+                created_rule._from_symbols.append(s)
+            #Copy childs to replace index
+            for i in range(proc.rule.replace_index):
+                ch = proc.rule.to_symbols[i]  # type: Nonterminal
+                ch._set_from_rule(created_rule)
+                created_rule._to_symbols.append(ch)
+            #Add epsilon
+            created_rule._to_symbols.append(EPSILON)
+            #Add rest of childs
+            for i in range(proc.rule.replace_index, len(proc.rule.to_symbols)):
+                ch = proc.rule.to_symbols[i]  # type: Nonterminal
+                ch._set_from_rule(created_rule)
+                created_rule._to_symbols.append(ch)
+    return root
