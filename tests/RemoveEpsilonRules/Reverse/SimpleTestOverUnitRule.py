165f82cb4f9e076f292b8a60dc31d7aba5852804
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-09-03 16:46:44 +0200

    Add test of restoring epsilon rules over unit rule

--- tests/RemoveEpsilonRules/Reverse/SimpleTestOverUnitRule.py
+++ tests/RemoveEpsilonRules/Reverse/SimpleTestOverUnitRule.py
@@ -16,8 +16,8 @@ class S(Nonterminal): pass
 class A(Nonterminal): pass
 class B(Nonterminal): pass
 class C(Nonterminal): pass
-class RuleS0B(Rule): rule = ([S], [0, B])
-class RuleA1B(Rule): rule = ([A], [1, B])
+class RuleS0A(Rule): rule = ([S], [0, A])
+class RuleAB(Rule): rule = ([A], [B])
 class RuleAEps(Rule): rule = ([A], [EPS])
 class RuleBEps(Rule): rule = ([B], [EPS])
 class RuleB1C(Rule): rule = ([B], [2, C])
@@ -35,16 +35,19 @@ class SimpleTest(TestCase):
     def test_simpleTest(self):
         g = Grammar(terminals=[0, 1, 2, 3],
                     nonterminals=[S, A, B, C],
-                    rules=[RuleS0B, RuleA1B, RuleAEps, RuleBEps, RuleB1C, RuleC11],
+                    rules=[RuleS0A, RuleAB, RuleAEps, RuleBEps, RuleB1C, RuleC11],
                     start_symbol=S)
         gr = ContextFree.transform_to_chomsky_normal_form(ContextFree.remove_unit_rules(ContextFree.remove_rules_with_epsilon(g)))
         pars = cyk(gr, [0])
         s = InverseContextFree.epsilon_rules_restore(InverseContextFree.unit_rules_restore(InverseContextFree.transform_from_chomsky_normal_form(pars)))
         self.assertIsInstance(s, S)
-        self.assertIsInstance(s.to_rule, RuleS0B)
+        self.assertIsInstance(s.to_rule, RuleS0A)
         self.assertIsInstance(s.to_rule.to_symbols[0], Terminal)
         self.assertEqual(s.to_rule.to_symbols[0].s, 0)
-        b = s.to_rule.to_symbols[1]
+        a = s.to_rule.to_symbols[1]
+        self.assertIsInstance(a, A)
+        self.assertIsInstance(a.to_rule, RuleAB)
+        b = a.to_rule.to_symbols[0]
         self.assertIsInstance(b, B)
         self.assertIsInstance(b.to_rule, RuleBEps)
         self.assertIs(b.to_rule.to_symbols[0], EPS)
