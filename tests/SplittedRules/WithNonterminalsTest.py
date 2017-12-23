7a886c3c95dbe70a881581ea1cee66be24378278
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-12-23 19:48:57 +0100

    Add test with nonterminal

--- tests/SplittedRules/WithNonterminalsTest.py
+++ tests/SplittedRules/WithNonterminalsTest.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 """
 :Author Patrik Valkovic
-:Created 23.12.2017 19:28
+:Created 23.12.2017 19:45
 :Licence GNUv3
 Part of grammpy-transforms
 
 """
+
 
 from unittest import main, TestCase
 from grammpy import *
@@ -14,32 +15,33 @@ from pyparsers import cyk
 
 
 class S(Nonterminal): pass
+class A(Nonterminal): pass
+class B(Nonterminal): pass
 class Rules(Rule):
     rules = [
-        ([S], [1]),
-        ([S], [0])
+        ([S], [A, B]),
+        ([A], [0]),
+        ([B], [1])
     ]
 
-class SimpleTest(TestCase):
-    def test_rewriteSimple0(self):
+class WithNonterminalsTest(TestCase):
+    def test_parseWithNonterminal(self):
         g = Grammar(terminals=[0, 1],
-                    nonterminals=[S],
+                    nonterminals=[S, A, B],
                     rules=[Rules],
                     start_symbol=S)
-        res = cyk(g, [0])
+        res = cyk(g, [0, 1])
         res = InverseCommon.splitted_rules(res)
         self.assertIsInstance(res, S)
         self.assertIsInstance(res.to_rule, Rules)
+        a = res.to_rule.to_symbols[0]
+        b = res.to_rule.to_symbols[1]
+        self.assertIsInstance(a, A)
+        self.assertIsInstance(b, B)
+        self.assertIsInstance(a.to_rule, Rules)
+        self.assertIsInstance(b.to_rule, Rules)
 
-    def test_rewriteSimple1(self):
-        g = Grammar(terminals=[0, 1],
-                    nonterminals=[S],
-                    rules=[Rules],
-                    start_symbol=S)
-        res = cyk(g, [1])
-        res = InverseCommon.splitted_rules(res)
-        self.assertIsInstance(res, S)
-        self.assertIsInstance(res.to_rule, Rules)
+
 
 
 if __name__ == '__main__':
